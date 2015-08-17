c105c926b394371d16a272d90fde92479f0a93f9
Dean Brettle <dean@brettle.com>
2015-08-17 15:06:36 -0700

    Create package to workaround meteor/meteor#4970.
    Workaround issue where login fail breaks auto-login on reconnect.

---
+++ README.md
@@ -0,0 +1,22 @@
+# brettle:workaround-issue-4970
+
+[![Build Status](https://travis-ci.org/brettle/meteor-accounts-workaround-issue-4970.svg?branch=master)](https://travis-ci.org/brettle/meteor-accounts-workaround-issue-4970)
+
+Workaround [Meteor issue #4970](https://github.com/meteor/meteor/issues/4970) so
+that a logged in user failing to login will not break auto-login on reconnect.
+
+## Installation
+
+```sh
+meteor add brettle:workaround-issue-4970
+```
+
+## Usage
+
+Just install it and it should work.
+
+## How it Works
+
+It registers an `onLogin` handler that remembers the current current value of
+`Accounts.connection.onReconnect` and an `onLoginFailure` handler that restores
+it after `Accounts.callLoginMethod()` erroneously sets it to `null`.
---
+++ package.js
@@ -0,0 +1,25 @@
+"use strict";
+
+Package.describe({
+  name: 'brettle:workaround-issue-4970',
+  version: '0.0.1',
+  summary:
+    'Workaround Meteor issue 4970 - login fail breaks auto-login on reconnect.',
+  git: 'https://github.com/brettle/meteor-workaround-issue-4970',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.4');
+  api.use('underscore', 'client');
+  api.use('brettle:workaround-issue-4331@0.0.1', 'client');
+  api.imply('accounts-base');
+  api.addFiles('workaround-issue-4970.js', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('brettle:accounts-testing-support');
+  api.use('brettle:workaround-issue-4970');
+  api.addFiles('workaround-issue-4970-tests.js', 'client');
+});
